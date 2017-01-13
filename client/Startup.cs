4f5902b02805d76f3bd775c40d7bbe7e3bc52a3d
Mike Harder <mharder@microsoft.com>
2017-01-12 18:08:42 -0800

    Apps to benchmark HttpClient

---
+++ client/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        private const int _threads = 64;
+
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        private static Stopwatch _stopwatch = Stopwatch.StartNew();
+        private static long _requests;
+
+        public static void Main(string[] args)
+        {
+            var url = args[0];
+
+            Console.WriteLine($"Requesting {url} with {_threads} threads...");
+            
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            WriteResults();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            RunTest(args[0]);
+        }
+
+        private static void RunTest(string url) {
+            var threadObjects = new Thread[_threads];
+
+            for (var i=0; i < _threads; i++) {
+                var thread = new Thread(() =>
+                {
+                    while (true) {
+                        using (var response = _httpClient.GetAsync(url).Result) { }
+                        Interlocked.Increment(ref _requests);
+                    }
+                });
+                threadObjects[i] = thread;
+                thread.Start();
+            }
+            
+            for (var i=0; i < _threads; i++) {
+                threadObjects[i].Join();
+            }
+        }
+
+        private static async Task WriteResults()
+        {
+            var lastRequests = (long)0;
+            var lastElapsed = TimeSpan.Zero;
+
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                var currentRequests = _requests - lastRequests;
+                lastRequests = _requests;
+
+                var elapsed = _stopwatch.Elapsed;
+                var currentElapsed = elapsed - lastElapsed;
+                lastElapsed = elapsed;
+
+                WriteResult(_requests, currentRequests, currentElapsed);
+            }
+        }
+
+        private static void WriteResult(long totalRequests, long currentRequests, TimeSpan elapsed)
+        {
+            Console.WriteLine($"{DateTime.UtcNow.ToString("o")}\tRequests\t{totalRequests}\tRPS\t{Math.Round(currentRequests / elapsed.TotalSeconds)}");
+        }        
+    }
+}
---
+++ server/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var hostBuilder = new WebHostBuilder()
+                .UseUrls("http://+:8080")
+                .UseKestrel()
+                .UseStartup<Startup>();
+            
+            var host = hostBuilder.Build();
+            host.Run();            
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("Hello from ASP.NET Core!");
+            });
+        }
+    }
+}
