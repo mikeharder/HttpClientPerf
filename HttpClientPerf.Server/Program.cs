fdbca5b1c42be3e6d72657c148db85f4c29e7a96
Mike Harder <mharder@microsoft.com>
2018-03-23 15:03:55 -0700

    Remove unused using

--- HttpClientPerf.Server/Program.cs
+++ HttpClientPerf.Server/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using System;
 using System.Diagnostics;
 using System.IO;
